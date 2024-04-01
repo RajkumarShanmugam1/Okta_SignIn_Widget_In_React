093830d5ce7e47268565938d5690d239d1203699
RajkumarShanmugam1 <sriraj15110@gmail.com>
2024-04-01 13:15:41 +0530

    update_git_pages

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from "./logo512.png"
 
 const Home = () => {
-
   return (
-    <div id="container">
+    <div id="container" className="d-flex justify-content-center align-items-center">
       <h1>Welcome OKTA Login</h1>
+      <img src={logo} className="App-logo" alt="logo" height={500} width={500}/>
     </div>
   );
 };
--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
+import { Card } from 'react-bootstrap';
 
 const Profile = () => {
   const { authState, oktaAuth } = useOktaAuth();
@@ -20,20 +21,41 @@ const Profile = () => {
 
   if (!userInfo) {
     return (
-      <div>
-        <p>Fetching user info ...</p>
+      <div className="container d-flex justify-content-center align-items-center">
+        <Card className="p-4">
+          <Card.Body>
+            <Card.Title id="welcome">Fetching user info ...</Card.Title>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
+  console.log(userInfo);
 
   return (
-    <div>
-      <div>
-        <p id="welcome">
-          Welcome, &nbsp;{userInfo.name}!
-        </p>
-        <p>You have successfully authenticated against your Okta org, and have been redirected back to this application.</p>
-      </div>
+    <div className="container d-flex justify-content-center align-items-center">
+      <Card className="p-4">
+        <Card.Body>
+          <Card.Title id="welcome">Welcome, {userInfo.name}!</Card.Title>
+          <br/>
+          <table className="table">
+            <tbody>
+              <tr>
+                <td>First Name</td>
+                <td>{userInfo.given_name}</td>
+              </tr>
+              <tr>
+                <td>Second Name</td>
+                <td>{userInfo.family_name}</td>
+              </tr>
+              <tr>
+                <td>Zone Info</td>
+                <td>{userInfo.zoneinfo}</td>
+              </tr>
+            </tbody>
+          </table>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
