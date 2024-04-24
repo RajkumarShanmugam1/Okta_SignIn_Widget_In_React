1efb14ca4ec45f11ffdb67cb6076c4b227c3289a
Rajkumar S <76644058+RajkumarShanmugam1@users.noreply.github.com>
2024-04-24 18:03:52 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const config = {
   oidc: {
-    issuer: 'https://dev-49584893.okta.com/oauth2/default',
-    clientId: '0oag28m1bz9GBcDny5d7',
+    issuer: '',
+    clientId: '',
     scopes: ['openid', 'profile', 'email'],
     redirectUri: 'http://localhost:3000/login/callback'
   },
   widget: {
-    issuer: 'https://dev-49584893.okta.com/oauth2/default',
-    clientId: '0oag28m1bz9GBcDny5d7',
+    issuer: '',
+    clientId: '',
     scopes: ['openid', 'profile', 'email'],
     redirectUri: 'http://localhost:3000/login/callback'
   }
